14e694c7d2d3654c510f244e7cdddab3144b9864
Isaac Falkenstine <isaac@falkenstine.com>
2018-11-06 18:11:09 -0700

    Added migration and fixed many tests

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -6,7 +6,6 @@ FactoryBot.define do
     sequence(:image) { |n| "https://picsum.photos/200/300?image=#{n}" }
     sequence(:price) { |n| ("#{n}".to_i+1)*1.5 }
     sequence(:inventory) { |n| ("#{n}".to_i+1)*2 }
-    sequence(:slug) { |n| "Name #{n}" }
     active { true }
   end
 
